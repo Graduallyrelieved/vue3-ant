c7e76bcd01f26d968d838445494748efb01f6538
wangkun <wangkun@axhome.com.cn>
2023-02-22 11:27:56 +0800

    first commit

--- src/views/her/index.vue
+++ src/views/her/index.vue
@@ -1,4 +1,21 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  const obj = {
+    name: '小明',
+    age: '12',
+  };
+  const keys = Object.keys(obj);
+  for (let i = 0; i < keys.length; i++) {
+    // console.log(obj[keys[i]]);
+  }
+
+  const arr = ['01', '02', '03'];
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i], JSON.parse(JSON.stringify(arr)), 'ooooooo');
+    if (arr[i] == '02') {
+      arr.push('04');
+    }
+  }
+</script>
 <template>
   <div>dd</div>
 </template>
